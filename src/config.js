4087097dd3457cf7f10c459470b0856e5a2a97c5
Dominion <limbotech116@gmail.com>
2025-11-18 13:15:00 +0100

    fix(config): update networks to use celo instead of arbitrum and mainnet

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 import { createAppKit } from "@reown/appkit/react";
 import { EthersAdapter } from "@reown/appkit-adapter-ethers";
-import { arbitrum, mainnet } from "@reown/appkit/networks";
+import { celo } from "@reown/appkit/networks";
 
 const projectId = "35c18b84fd9706f0b560fe2ae76bf72c";
 
-const networks = [arbitrum, mainnet];
+const networks = [celo];
 
 const metadata = {
   name: "My Website",
